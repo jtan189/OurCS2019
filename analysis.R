879aaeba34b6bafcb191c7c30f91acaac56ac136
Josh Tan <jtan189@gmail.com>
2019-10-19 10:06:10 -0400

    Added data prep script

---
+++ analysis.R
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+
+source('prepare_data.R')
+
+## TODO: perform analysis
+
+cat("Hello OurCS!\n")
---
+++ prepare_data.R
@@ -0,0 +1,161 @@
+#!/usr/bin/env Rscript
+##
+## In an interactive R session or from another R script, run using:
+## > source('prepare_data.R')
+
+options(error=recover)
+
+## install packages with, e.g.:
+## > install.packages("dplyr")
+
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(ggplot2))
+
+kSurveyFilename <- "data/Survey Results.csv"
+
+kSurveyColnames <- c(
+    "start.date",
+    "end.date",
+    "response.type",
+    "ip.address",
+    "progress",
+    "duration.sec",
+    "finished",
+    "recorded.date",
+    "response.id",
+    "recipient.last.name",
+    "recipient.first.name",
+    "recipient.email",
+    "external.data.reference",
+    "location.latitude",
+    "location.longitude",
+    "distribution.channel",
+    "user.language",
+    "month",
+    "year",
+    "times.use.device.in.day",
+    "use.device.for.audio",
+    "use.device.for.info",
+    "use.device.for.control.other.devices",
+    "use.device.for.conversation",
+    "use.device.for.shopping",
+    "use.device.for.alarm",
+    "use.device.for.weather",
+    "use.device.for.time",
+    "use.device.for.reminders",
+    "use.device.for.other",                        
+    "use.device.for.other.text",
+    "initial.privacy.concerns",
+    "current.privacy.concerns",
+    "what.privacy.concerns",
+    "mic.on.by.default",
+    "recordings.stored.by.company",
+    "can.access.command.history",
+    "device.option.delete.data",
+    "no.device.privacy.settings",
+    "age",
+    "gender",
+    "gender.self.describe",
+    "work.it.or.cs.degree",
+    "education.level"
+)
+
+
+survey.data <- read.csv(kSurveyFilename,
+                        header=FALSE,
+                        skip=3,
+                        stringsAsFactors=FALSE,
+                        col.names=kSurveyColnames)
+
+survey.data <- survey.data %>%
+    mutate(start.date = as.POSIXct(start.date),
+           end.date = as.POSIXct(end.date),
+           recorded.date = as.POSIXct(recorded.date)) %>%
+    filter(response.type == "IP Address") %>%
+    select(-response.type,
+           -progress,
+           -finished,
+           -ip.address,
+           -recipient.last.name,
+           -recipient.first.name,
+           -recipient.email,
+           -external.data.reference,
+           -location.latitude,
+           -location.longitude,
+           -distribution.channel,
+           -user.language,
+           -gender.self.describe)
+
+survey.data$month <- factor(survey.data$month, levels=1:12)
+survey.data$year <- as.factor(survey.data$year)
+survey.data$times.use.device.in.day <- as.factor(survey.data$times.use.device.in.day)
+
+survey.data$use.device.for.audio <- survey.data$use.device.for.audio != ""
+survey.data$use.device.for.info <- survey.data$use.device.for.info != ""
+survey.data$use.device.for.conversation <- survey.data$use.device.for.conversation != ""
+survey.data$use.device.for.control.other.devices <- survey.data$use.device.for.control.other.devices != ""
+survey.data$use.device.for.shopping <- survey.data$use.device.for.shopping != ""
+survey.data$use.device.for.alarm <- survey.data$use.device.for.alarm != ""
+survey.data$use.device.for.weather <- survey.data$use.device.for.weather != ""
+survey.data$use.device.for.time <- survey.data$use.device.for.time != ""
+survey.data$use.device.for.reminders <- survey.data$use.device.for.reminders != ""
+survey.data$use.device.for.other <- survey.data$use.device.for.other != ""
+
+survey.data$gender <- as.factor(survey.data$gender)
+survey.data$education.level <- as.factor(survey.data$education.level)
+
+kLikertAgree <- c(
+    "Strongly disagree",
+    "Somewhat disagree",
+    "Neither agree nor disagree",
+    "Somewhat agree",
+    "Strongly agree"
+)
+
+survey.data$initial.privacy.concerns <- factor(survey.data$initial.privacy.concerns,
+                                               levels=kLikertAgree,
+                                               ordered=TRUE)
+survey.data$current.privacy.concerns <- factor(survey.data$current.privacy.concerns,
+                                               levels=kLikertAgree,
+                                               ordered=TRUE)
+
+kLikertCorrect <- c(
+    "Definitely incorrect",
+    "Probably incorrect",
+    "I don't know",
+    "Probably correct",
+    "Definitely correct"
+)
+
+survey.data$mic.on.by.default <- factor(survey.data$mic.on.by.default,
+                                       levels=kLikertCorrect,
+                                       ordered=TRUE)
+
+survey.data$recordings.stored.by.company <- factor(survey.data$recordings.stored.by.company,
+                                                   levels=kLikertCorrect,
+                                                   ordered=TRUE)
+
+survey.data$can.access.command.history <- factor(survey.data$can.access.command.history,
+                                                 levels=kLikertCorrect,
+                                                 ordered=TRUE)
+
+survey.data$device.option.delete.data <- factor(survey.data$device.option.delete.data,
+                                                levels=kLikertCorrect,
+                                                ordered=TRUE)
+
+survey.data$no.device.privacy.settings <- factor(survey.data$no.device.privacy.settings,
+                                                 levels=kLikertCorrect,
+                                                 ordered=TRUE)
+kAge <- c("18 - 24",
+          "25 - 34",
+          "35 - 44",
+          "45 - 54",
+          "55 - 64")
+
+survey.data$age <- factor(survey.data$age,
+                          levels=kAge,
+                          ordered=TRUE)
+
+survey.data$work.it.or.cs.degree <- factor(survey.data$work.it.or.cs.degree,
+                                                 levels=c("Yes", "No"))
+
